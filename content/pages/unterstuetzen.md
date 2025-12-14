9647ab9aac1c95066994e9f098d472cfa5c07481
cizz <cizz@gmx.net>
2025-12-14 11:08:25 +0100

    V2_sponsoring

--- content/pages/unterstuetzen.md
+++ content/pages/unterstuetzen.md
@@ -3,35 +3,6 @@ slug: support
 title: Unterstützen
 ---
 
-# Unterstützung
-
-Die Teilnehmenden werden in kurzen Workshops mit Design Thinking, Prototyping und Vibe Coding mit KI vertraut gemacht. So entstehen schnell greifbare Ideen – auch ohne tiefere Programmierkenntnisse.
-Sie möchten den Hackathon 2026 unterstützen?
-
-##Sachsponsoring: Thema einbringen
-Als Themenpartner können Sie eine konkrete Fragestellung oder Herausforderung aus Ihrem Unternehmen einbringen. Die Teams bearbeiten diese praxisnah und anwendungsorientiert und liefern frische Perspektiven auf Prozesse oder Produkte. Am **12. oder 15.05.2026** bitten wir Sie, sich etwas Zeit für die Teilnehmenden zu nehmen, um ihnen Fragen zu dem Thema zu beantworten, die sie aufgrund der Workshops, die im Voraus stattfinden, haben werden.
-
-##Finanzsponsoring: Hackathon ermöglichen
-Mit einem finanziellen Beitrag unterstützen Sie die Durchführung des Hackathons, insbesondere Printwerbung und Catering am Veranstaltungstag.
-Dafür bieten wir Sponsoringpakete mit Sichtbarkeit:
-
-- online (Webseite, Kommunikation)
-
-- vor Ort am Hackathon-Tag
-
-Die genauen Leistungen stimmen wir individuell ab.
-
-Wichtig: Sollte der Hackathon wider Erwarten nicht stattfinden, werden bereits gezahlte Sponsoringbeträge selbstverständlich vollständig zurückerstattet.
-
-**Jetzt unterstützen!**
-Ob mit einem Thema oder als finanzieller Sponsor – Sie kommen mit engagierten Nachwuchstalenten in Kontakt und positionieren Ihr Unternehmen in einem innovativen Umfeld.
-
-*Interesse?*
-Schreiben Sie uns gern an hackathon@digitale-oberlausitz.eu – wir freuen uns auf den Austausch!
-
-
-
-
 Werden sie [Vereinsmitglied](https://digitale-oberlausitz.eu/mitmachen) (als Privatperson)
 oder [Fördermitglied](https://digitale-oberlausitz.eu/foerdermitglieder) (als Unternehmen)!
 
--- src/pages/sponsoring.tsx
+++ src/pages/sponsoring.tsx
@@ -15,12 +15,32 @@ const SponsoringPage: React.FC<PageProps> = () => {
                         children={`
 # Wir sind auf der Suche nach weiteren Sponsoren
 
-Wir sind auf der Suche nach Sponsoren, die unsere Veranstaltung finanziell unterstützen möchten.
+# Unterstützung
 
-Wenn Sie unsere Vision teilen, unterstützen Sie uns! Das eingeworbene Geld wird vollständig für die 
-Organisation des Hackathons sowie die Finanzierung von Preisen für die Teilnehmenden verwendet.
+Die Teilnehmenden werden in kurzen Workshops mit Design Thinking, Prototyping und Vibe Coding mit KI vertraut gemacht. So entstehen schnell greifbare Ideen – auch ohne tiefere Programmierkenntnisse.
+Sie möchten den Hackathon 2026 unterstützen?
 
-**Melden Sie sich bei Fragen gerne unter [hackathon@digitale-oberlausitz.eu](mailto:hackathon@digitale-oberlausitz.eu)**
+##Sachsponsoring: Thema einbringen
+Als Themenpartner können Sie eine konkrete Fragestellung oder Herausforderung aus Ihrem Unternehmen einbringen. Die Teams bearbeiten diese praxisnah und anwendungsorientiert und liefern frische Perspektiven auf Prozesse oder Produkte. Am **12. oder 15.05.2026** bitten wir Sie, sich etwas Zeit für die Teilnehmenden zu nehmen, um ihnen Fragen zu dem Thema zu beantworten, die sie aufgrund der Workshops, die im Voraus stattfinden, haben werden.
+
+##Finanzsponsoring: Hackathon ermöglichen
+Mit einem finanziellen Beitrag unterstützen Sie die Durchführung des Hackathons, insbesondere Printwerbung und Catering am Veranstaltungstag.
+Dafür bieten wir Sponsoringpakete mit Sichtbarkeit:
+
+## Gold-Paket (1024 Euro)
+- alles aus Silber-Paket
+- Sponsor kann sich auf Eröffnungsveranstaltung selbst vorstellen (5 Min)
+- stellen eines Jury-Mitglieds (Details des Jury-Prozesses folgen)
+- Zitat/Grußwort auf der Website (begrenzte Zeichenzahl, Foto von Personen + Logo möglich, Darstellung im Stile von Testimonials)
+- aufstellen eines Rollups (muss von Sponsor bereitgestellt werden)
+- großes Logo auf Sponsoring-Poster oder digitaler Sponsoren-Präsentation
+
+## Silber-Paket (512 Euro)
+- alles aus Bronze-Paket
+- Zugang zu Job-Wall (Pinnwand) bei der Veranstaltung
+- Aufhängen von eigenen Plakaten (in Abstimmung mit uns)
+- mittleres Logo auf Sponsoren-Poster oder digitaler Sponsoren-Präsentation
+- Trailer/Poster auf Bildschirm-Wand im Durchlauf
 
 ## Bronze-Paket  (256 Euro)
 
@@ -31,20 +51,20 @@ Organisation des Hackathons sowie die Finanzierung von Preisen für die Teilnehm
 - kleines Logo auf Sponsoren-Poster oder digitaler Sponsoren-Präsentation
 - Erwähnung mit Logo auf Werbe-Plakat (zeitlich befristed bis 31.11.2026)
 
-## Silber-Paket (512 Euro)
-- alles aus Bronze-Paket
-- Zugang zu Job-Wall (Pinnwand) bei der Veranstaltung
-- Aufhängen von eigenen Plakaten (in Abstimmung mit uns)
-- mittleres Logo auf Sponsoren-Poster oder digitaler Sponsoren-Präsentation
-- Trailer/Poster auf Bildschirm-Wand im Durchlauf
+**Wichtig: Sollte der Hackathon wider Erwarten nicht stattfinden, werden bereits gezahlte Sponsoringbeträge selbstverständlich vollständig zurückerstattet.**
 
-## Gold-Paket (1024 Euro)
-- alles aus Silber-Paket
-- Sponsor kann sich auf Eröffnungsveranstaltung selbst vorstellen (5 Min)
-- stellen eines Jury-Mitglieds (Details des Jury-Prozesses folgen)
-- Zitat/Grußwort auf der Website (begrenzte Zeichenzahl, Foto von Personen + Logo möglich, Darstellung im Stile von Testimonials)
-- aufstellen eines Rollups (muss von Sponsor bereitgestellt werden)
-- großes Logo auf Sponsoring-Poster oder digitaler Sponsoren-Präsentation
+**Jetzt unterstützen!**
+Ob mit einem Thema oder als finanzieller Sponsor – Sie kommen mit engagierten Nachwuchstalenten in Kontakt und positionieren Ihr Unternehmen in einem innovativen Umfeld.
+
+*Interesse?*
+Schreiben Sie uns gern an hackathon@digitale-oberlausitz.eu – wir freuen uns auf den Austausch!
+
+Wir sind auf der Suche nach Sponsoren, die unsere Veranstaltung finanziell unterstützen möchten.
+
+Wenn Sie unsere Vision teilen, unterstützen Sie uns! Das eingeworbene Geld wird vollständig für die 
+Organisation des Hackathons sowie die Finanzierung von Preisen für die Teilnehmenden verwendet.
+
+**Melden Sie sich bei Fragen gerne unter [hackathon@digitale-oberlausitz.eu](mailto:hackathon@digitale-oberlausitz.eu)**
             `}
                     />
                 </>
