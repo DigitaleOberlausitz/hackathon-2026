6d4d6dc9983a3b086a2867fbbb6daf6e77e89fdd
Max Wielsch <max.wielsch@digitale-oberlausitz.eu>
2026-03-03 20:40:35 +0100

    Fix link to 2024 hackathon.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -69,7 +69,7 @@ export const Navbar: FC = () => (
             <p>Archiv</p>
             <ul>
                 <li>
-                    <a href="https://hackathon20243.digitale-oberlausitz.eu">2024</a>
+                    <a href="https://hackathon2024.digitale-oberlausitz.eu">2024</a>
                 </li>
                 <li>
                     <a href="https://hackathon2023.digitale-oberlausitz.eu">2023</a>
